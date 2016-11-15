b005e3356a74ddd46f00902f366aa28e90a5bcc3
williamsjoe <jwj.robert@gmail.com>
2016-11-15 15:00:39 -0500

    Add client WS connection

---
+++ public/ws.js
@@ -0,0 +1,9 @@
+console.log('Start App');
+
+// var HOST = location.origin.replace(/^http/, 'ws');
+// var ws = new WebSocket(HOST);
+// var el = document.getElementById('server-time');
+
+// ws.onmessage = function (event) {
+// 	el.innerHTML =  'Server time: ' + event.data;
+// };
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const SocketServer = require('ws').Server;
+const path = require('path');
 
 const server = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+const INDEX = path.join(__dirname, '/');
 
 server.use((req, res) => res.sendFile(INDEX))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new SocketServer({server});
+
+wss.on('connection', (ws) => {
+	console.log('Client Connected');
+	ws.on('close', () => console.log('Client Disconnected'));
+});
+
+setInterval(() => {
+	wss.clients.forEach((client) => {
+		client.send(new Date().toTimeString());
+	})
+}, 1000);
